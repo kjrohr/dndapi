a171fa5358c9af80b21a896c5b222b5a6a7cb575
Karl Rohr <kjrohr@fullsail.edu>
2016-08-13 17:32:37 -0400

    Fixed api.js

--- src/routes/api.js
+++ src/routes/api.js
@@ -1,18 +1,15 @@
-const util = require('../../lib/util');
 // Starts the export for this file.
 module.exports = (express) => {
   // Makes an instance of express.Router() and assigns it to router
-  let router = express.Router();
+  const router = express.Router();
 
   // /api/ route will return { hello: 'world'}
-  router.get('/', (req,res) => {
-    res.json({ hello: 'world',
-
-   });
+  router.get('/', (req, res) => {
+    res.json({ hello: 'world' });
   });
 
   // /api/status route will return { healty: 'true'}
-  router.get('/status', (req,res) => {
+  router.get('/status', (req, res) => {
     res.json({ healty: true });
   });
 
