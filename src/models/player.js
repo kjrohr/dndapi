857eebe27e52019c3254a05fb5ffc06cad1ab790
Karl Rohr <kjrohr@fullsail.edu>
2016-08-13 17:31:13 -0400

    Fixed player.js it has the same potential error as guild.js

--- src/models/player.js
+++ src/models/player.js
@@ -1,23 +1,25 @@
 // // Makes an instance of db.js and sets it to variable db
-const db = require('./db.js');
+// WARNING: Same issue as guild.js
+const db = require('./db');
+const util = require('../../lib/util');
 
 // Exports the add function which references the sequelize create method.
 exports.add = (payload, err, success) => {
   db.player.create(payload).then(success).catch(err);
   util.debug('Player Model - Add a Player', payload.name + '\n' + payload.description);
-}
+};
 
 // Exports the all function which references the sequelize findAll method
 exports.all = (err, success) => {
   db.player.findAll().then(success).catch(err);
   util.debug('Player Model - Find all Players', success);
-}
+};
 
 // Exports the one funciton which references the sequelize find method
 exports.one = (payload, err, success) => {
   db.player.find({
     where: {
-      id: payload.id
+      id: payload.id,
     },
     // Find all relations defined in sequelize
     include: [{
@@ -26,27 +28,27 @@ exports.one = (payload, err, success) => {
     }],
   }).then(success).catch(err);
   util.debug('Player Model - Find a Player', 'id: ' + payload.id);
-}
+};
 
 // Exports remove which references the sequelize destroy method
 exports.remove = (payload, err, success) => {
   db.player.destroy({
     where: {
       id: payload.id,
-    }
+    },
   }).then(success).catch(err);
   util.debug('Player Model - Delete a Player', 'id: ' + payload.id);
-}
+};
 
 // Exports updates which references the sequelize find method
 // then takes the existing Data and updates the attributes
 exports.update = (payload, err, success) => {
-    db.player.find({
-      where: {
-        id: payload.id,
-      }
-    }).then((existingData) => {
-      existingData.updateAttributes(payload).then(success).catch(err);
-    }).catch(err);
-    util.debug('Player Model - Update a Player', 'id: ' + payload.id);
-}
+  db.player.find({
+    where: {
+      id: payload.id,
+    },
+  }).then((existingData) => {
+    existingData.updateAttributes(payload).then(success).catch(err);
+  }).catch(err);
+  util.debug('Player Model - Update a Player', 'id: ' + payload.id);
+};
