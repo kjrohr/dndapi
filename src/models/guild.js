a9baa6930a160218ee3b75b829230a8c2fd2fd5f
Karl Rohr <kjrohr@fullsail.edu>
2016-08-13 17:29:05 -0400

    Fixed guild, there is a possible error of the db file messing up now, check comments

--- src/models/guild.js
+++ src/models/guild.js
@@ -1,24 +1,25 @@
 // Makes an instance of db.js and sets it to variable db
-const db = require('./db.js');
+// WARNING: Not having the .js after db broke it once, check again
+const db = require('./db');
 const util = require('../../lib/util');
 
 // Exports the add function which references the sequelize create method.
 exports.add = (payload, err, success) => {
   db.guild.create(payload).then(success).catch(err);
   util.debug('Guild Model - Add a Guild', payload.name + '\n' + payload.description);
-}
+};
 
 // Exports the all function which references the sequelize findAll method
 exports.all = (err, success) => {
   db.guild.findAll().then(success).catch(err);
   util.debug('Guild Model - Find all Guilds', success);
-}
+};
 
 // Exports the one funciton which references the sequelize find method
 exports.one = (payload, err, success) => {
   db.guild.find({
     where: {
-      id: payload.id
+      id: payload.id,
     },
     // Find all relations defined in sequelize
     include: [{
@@ -27,27 +28,27 @@ exports.one = (payload, err, success) => {
     }],
   }).then(success).catch(err);
   util.debug('Guild Model - Find a Guild', 'id: ' + payload.id);
-}
+};
 
 // Exports remove which references the sequelize destroy method
 exports.remove = (payload, err, success) => {
   db.guild.destroy({
     where: {
       id: payload.id,
-    }
+    },
   }).then(success).catch(err);
   util.debug('Guild Model - Delete a Guild', 'id: ' + payload.id);
-}
+};
 
 // Exports updates which references the sequelize find method
 // then takes the existing Data and updates the attributes
 exports.update = (payload, err, success) => {
-    db.guild.find({
-      where: {
-        id: payload.id,
-      }
-    }).then((existingData) => {
-      existingData.updateAttributes(payload).then(success).catch(err);
-    }).catch(err);
-    util.debug('Guild Model - Update a Guild', 'id: ' + payload.id);
-}
+  db.guild.find({
+    where: {
+      id: payload.id,
+    },
+  }).then((existingData) => {
+    existingData.updateAttributes(payload).then(success).catch(err);
+  }).catch(err);
+  util.debug('Guild Model - Update a Guild', 'id: ' + payload.id);
+};
