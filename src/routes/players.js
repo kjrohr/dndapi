a066e77eeaf13a9b22c2422230e1678405296152
Karl Rohr <kjrohr@fullsail.edu>
2016-08-13 17:36:11 -0400

    Mostly fixed players.js

--- src/routes/players.js
+++ src/routes/players.js
@@ -1,13 +1,12 @@
-const player = require('../models/player.js');
+const player = require('../models/player');
 
 // Starts the export for this file
 module.exports = (express) => {
   // Makes a new instance of express.Router() and assigns it to router.
-  let router = express.Router();
+  const router = express.Router();
 
   // Read All
-  router.get('/players', (req,res) => {
-    console.log('finding all');
+  router.get('/players', (req, res) => {
     player.all((err) => {
       res.status(500).json(err);
     }, (data) => {
@@ -16,43 +15,45 @@ module.exports = (express) => {
   });
 
   // Read All
-  router.get('/players/:id', (req,res) => {
+  router.get('/players/:id', (req, res) => {
+    // WARNING: Look at prior assignment to the answer for this.
     req.body.id = req.params.id;
     player.one(req.body, (err) => {
       res.status(500).json(err);
     }, (data) => {
       res.status(200).json(data);
-    })
+    });
   });
 
   // Delete
-  router.delete('/players/:id', (req,res) => {
+  router.delete('/players/:id', (req, res) => {
+    // WARNING: Look at prior assignment to the answer for this.
     req.body.id = req.params.id;
     player.remove(req.body, (err) => {
       res.status(500).json(err);
     }, (data) => {
       res.status(200).json(data);
-    })
+    });
   });
 
   // Create
-  router.post('/players', (req,res) => {
-    console.log('create player hit');
+  router.post('/players', (req, res) => {
     player.add(req.body, (err) => {
       res.status(500).json(err);
     }, (data) => {
       res.status(200).json(data);
-    })
+    });
   });
 
   // Update
-  router.post('/players/:id', (req,res) => {
+  router.post('/players/:id', (req, res) => {
+    // WARNING: Look at prior assignment to the answer for this.
     req.body.id = req.params.id;
     player.update(req.body, (err) => {
       res.status(500).json(err);
     }, (data) => {
       res.status(200).json(data);
-    })
+    });
   });
 
 // Returns router to the file that would call it
