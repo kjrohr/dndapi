3334dd8e0be55649a16a830eacd4442dbde674d1
Karl Rohr <kjrohr@fullsail.edu>
2016-08-16 21:53:34 -0400

    Added utility tool to guild routes

--- src/routes/guilds.js
+++ src/routes/guilds.js
@@ -1,4 +1,5 @@
 const guild = require('../models/guild');
+const util = require('../../lib/util');
 // Starts the export for this file
 module.exports = (express) => {
   // Makes a new instance of express.Router() and assigns it to router.
@@ -8,19 +9,24 @@ module.exports = (express) => {
   router.get('/guilds', (req, res) => {
     guild.all((err) => {
       res.status(500).json(err);
+      util.debug('ERROR - GET ALL - /api/v1/guilds', err);
     }, (data) => {
       res.status(200).json(data);
+      util.debug('SUCCESS - GET ALL - /api/v1/guilds', data);
     });
   });
 
-  // Read All
+  // Read One
   router.get('/guilds/:id', (req, res) => {
     // WARNING: Look at prior assignment to the answer for this.
     req.body.id = req.params.id;
     guild.one(req.body, (err) => {
       res.status(500).json(err);
+      util.debug('ERROR - GET ONE - /api/v1/guilds/:id', err);
     }, (data) => {
       res.status(200).json(data);
+      util.debug('SUCCESS - GET ONE - /api/v1/guilds/:id', 'id: ' + data.id + '\nname: '
+      + data.name + '\ndescription: ' + data.description + '\nfaction: ' + data.factionId);
     });
   });
 
@@ -30,8 +36,10 @@ module.exports = (express) => {
     req.body.id = req.params.id;
     guild.remove(req.body, (err) => {
       res.status(500).json(err);
+      util.debug('ERROR - DELETE - /api/v1/guilds/:id', err);
     }, (data) => {
       res.status(200).json(data);
+      util.debug('SUCCESS - DELETE - /api/v1/guilds/:id', 'Success?: ' + data);
     });
   });
 
@@ -40,8 +48,11 @@ module.exports = (express) => {
     // WARNING: Look at prior assignment to the answer for this.
     guild.add(req.body, (err) => {
       res.status(500).json(err);
+      util.debug('ERROR - POST - /api/v1/guilds', err);
     }, (data) => {
       res.status(200).json(data);
+      util.debug('SUCCESS - POST - /api/v1/guilds', 'id: ' + data.id + '\nname: '
+      + data.name + '\ndescription: ' + data.description + '\nfaction: ' + data.factionId);
     });
   });
 
@@ -51,9 +62,11 @@ module.exports = (express) => {
     const guildData = { id: req.params.id };
     guild.one(guildData, (err) => {
       res.status(500).json(err);
+      util.debug('ERROR - GET PLAYERS FROM GUILD - /api/guilds/:id/players', err);
     }, (data) => {
       // By putting .guilds when you go to the specific route it will only show the guilds.
       res.status(200).json(data.players);
+      util.debug('SUCCESS - GET PLAYERS FROM GUILD - /api/guilds/:id/players', data.players);
     });
   });
 
@@ -63,8 +76,11 @@ module.exports = (express) => {
     req.body.id = req.params.id;
     guild.update(req.body, (err) => {
       res.status(500).json(err);
+      util.debug('ERROR - POST UPDATE - /api/v1/guilds/:id', err);
     }, (data) => {
       res.status(200).json(data);
+      util.debug('SUCCESS - POST UPDATE - /api/v1/guilds/:id', 'id: ' + data.id + '\nname: '
+      + data.name + '\ndescription: ' + data.description + '\nfaction: ' + data.factionId);
     });
   });
 
