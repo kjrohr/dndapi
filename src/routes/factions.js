204544a6467a69a7fd64306033adab2a8740221b
Karl Rohr <kjrohr@fullsail.edu>
2016-08-16 21:39:50 -0400

    Added utility tool to faction route file

--- src/routes/factions.js
+++ src/routes/factions.js
@@ -1,4 +1,5 @@
 const faction = require('../models/faction');
+const util = require('../../lib/util');
 // Starts the export for this file
 module.exports = (express) => {
   // Makes a new instance of express.Router() and assigns it to router.
@@ -8,19 +9,23 @@ module.exports = (express) => {
   router.get('/factions', (req, res) => {
     faction.all((err) => {
       res.status(500).json(err);
+      util.debug('ERROR - GET ALL - /api/v1/factions', err);
     }, (data) => {
       res.status(200).json(data);
     });
   });
 
-  // Read All
+  // Read One
   router.get('/factions/:id', (req, res) => {
     // WARNING: Look at prior assignment to the answer for this.
     req.body.id = req.params.id;
     faction.one(req.body, (err) => {
       res.status(500).json(err);
+      util.debug('ERROR - GET ONE- /api/v1/factions/:id', err);
     }, (data) => {
       res.status(200).json(data);
+      util.debug('SUCCESS - GET ONE - /api/v1/factions/:id', 'id: ' + data.id
+      + '\nname: ' + data.name);
     });
   });
 
@@ -30,8 +35,10 @@ module.exports = (express) => {
     req.body.id = req.params.id;
     faction.remove(req.body, (err) => {
       res.status(500).json(err);
+      util.debug('ERROR - DELETE ONE - /api/v1/factions/:id', err);
     }, (data) => {
       res.status(200).json(data);
+      util.debug('SUCCESS - DELETE ONE - /api/v1/factions/:id', 'Success?' + data);
     });
   });
 
@@ -40,20 +47,26 @@ module.exports = (express) => {
     // WARNING: Look at prior assignment to the answer for this.
     faction.add(req.body, (err) => {
       res.status(500).json(err);
+      util.debug('ERROR - POST - /api/v1/factions', err);
     }, (data) => {
       res.status(200).json(data);
+      util.debug('SUCCESS - POST - /api/v1/factions', 'id: ' + data.id
+      + '\nname: ' + data.name);
     });
   });
 
-  // Read One
+  // Read Guilds
   // got to the route of a specificed player faction
   router.get('/factions/:id/guilds', (req, res) => {
     const factionData = { id: req.params.id };
     faction.one(factionData, (err) => {
       res.status(500).json(err);
+      util.debug('ERROR - GET ONE - /api/v1/factions/:id', err);
     }, (data) => {
       // By putting .factions when you go to the specific route it will only show the factions.
       res.status(200).json(data.guilds);
+      util.debug('SUCCESS - GET ONE - /api/v1/factions/:id', 'id: ' + data.id
+      + '\nname: ' + data.name);
     });
   });
 
@@ -61,9 +74,11 @@ module.exports = (express) => {
     const factionData = { id: req.params.id };
     faction.one(factionData, (err) => {
       res.status(500).json(err);
+      util.debug('ERROR - GET PLAYERS - /api/v1/factions/:id/players', err);
     }, (data) => {
       // By putting .factions when you go to the specific route it will only show the factions.
       res.status(200).json(data.players);
+      util.debug('SUCCESS - GET PLAYERS - /api/v1/factions/:id/players', data.players);
     });
   });
 
@@ -73,8 +88,11 @@ module.exports = (express) => {
     req.body.id = req.params.id;
     faction.update(req.body, (err) => {
       res.status(500).json(err);
+      util.debug('ERROR - POST UPDATE FACTION - /api/v1/factions/:id', err);
     }, (data) => {
       res.status(200).json(data);
+      util.debug('SUCCESS - POST UPDATE FACTION - /api/v1/factions/:id', 'id: ' + data.id
+      + '\nname: ' + data.name);
     });
   });
 
