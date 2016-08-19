8c98e7c0e34e391ef8a185040e0185e6e00b45f6
Karl Rohr <kjrohr@fullsail.edu>
2016-08-18 20:37:34 -0400

    Added guilds route test

---
+++ test/__routes__guilds.js
@@ -0,0 +1,46 @@
+const expect = require('chai').expect;
+const request = require('supertest');
+
+describe('Guild Routes', () => {
+  let server;
+  let guild;
+
+  beforeEach(() => {
+    server = require('../src/server');
+  });
+
+  afterEach(() => {
+    server.close();
+  });
+
+  // Test for Multiple Guilds
+  it('GET /api/v1/guilds returns multiple guilds', (done) => {
+    request(server)
+      .get('/api/v1/guilds')
+      .set('Accept', 'guildlication/json')
+      .expect('Content-Type', /json/)
+      .expect((res) => {
+        const guilds = res.body;
+
+        // Save one single guild from the list to test on in later tests
+        this.guild = guilds[0];
+
+        expect(guilds.length).to.be.above(0);
+      })
+      .end(done);
+  });
+
+  // Test for a single guild
+  it('GET /api/v1/guilds/:id returns an guild obj with id, title, description, and releaseDate properties', (done) => {
+    request(server)
+      .get('/api/v1/guilds/' + this.guild.id)
+      .set('Accept', 'guildlication/json')
+      .expect('Content-Type', /json/)
+      .expect((res) => {
+        const guild = res.body;
+        expect(guild).to.have.property('id');
+        expect(guild).to.have.property('name');
+      })
+      .end(done);
+  });
+});
